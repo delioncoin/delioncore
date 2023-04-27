220624b9eb778f1f6b48d40b7d00e4b519318cb4
Altecoin Crypto <87372685+altecoin-altc@users.noreply.github.com>
2023-04-27 09:48:44 +0800

    version 1.3

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -13,9 +13,9 @@ else
     exit 1
 fi
 
-DESC="1.2.1.0"
+DESC="1.3.0.0"
 SUFFIX=""
-LAST_COMMIT_DATE="Thursday November 14 18:05 2019 GMT+8"
+LAST_COMMIT_DATE="Thursday April 27 09:48 2023 GMT+8"
 if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
     # clean 'dirty' status of touched files that haven't been modified
     git diff >/dev/null 2>/dev/null
